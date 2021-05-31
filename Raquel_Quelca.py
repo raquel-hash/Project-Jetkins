764b41f5fe92f24c91add48f9e77f0d846fe6e9c
rachel-hash <quelcaraquel1@gmail.com>
2021-05-31 18:14:49 -0400

    subiendo un linea de codigo print

--- Raquel_Quelca.py
+++ Raquel_Quelca.py
@@ -1,10 +1,11 @@
 print("Ana Raquel Quelca Cadena")
 
-def sumList(NumberList){
-    result =0
-    for i in NumberList
-        result=result+i
-    return result
-}
+#def sumList(NumberList){
+#   result =0
+#    for i in NumberList
+#        result=result+i
+#    return result
+#}
 
-print(sumList([1,2,3,4,5]))
+#print(sumList([1,2,3,4,5]))
+print("What's your name")
